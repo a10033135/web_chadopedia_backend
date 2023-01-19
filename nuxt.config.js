a50d5dbfcc1ea3b2513ae53998c7d88ca0bbcbdb
Evan.Hsu <evan.hsu@gomaji.com>
2023-01-19 18:15:09 +0800

    remove emulator

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,16 +56,11 @@ export default {
               onAuthStateChangedMutation: 'ON_AUTH_STATE_CHANGED_MUTATION'
             },
             ssr: true, // default
-            emulatorPort: 9091,
-            emulatorHost: 'http://127.0.0.1',
-            disableEmulatorWarnings: false,
           },
           firestore: {
             memoryOnly: false, // default
             chunkName: process.env.NODE_ENV !== 'production' ? 'firebase-auth' : '[id]', // default
             enablePersistence: true,
-            emulatorPort: 9092,
-            emulatorHost: 'localhost',
           },
           functions: true,
           storage: true,
